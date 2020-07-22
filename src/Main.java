2cfd21a0739b66ad286dae3b01a88359050974fe
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-22 08:30:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * Created by KPS on 7/22/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        String str1 = "This is exercise 1";
+        String str2 = "This is Exercise 1";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+
+        // Compare the two strings.
+        int result = str1.compareToIgnoreCase(str2);
+
+        // Display the results of the comparison.
+        if (result < 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is less than " +
+                    "\"" + str2 + "\"");
+        }
+        else if (result == 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is equal to " +
+                    "\"" + str2 + "\"");
+        }
+        else // if (result > 0)
+        {
+            System.out.println("\"" + str1 + "\"" +
+                    " is greater than " +
+                    "\"" + str2 + "\"");
+        }
+    }
+}
